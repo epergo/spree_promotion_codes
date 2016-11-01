cae3009d547704ef9107877738a3d171d9b16063
epergo <ej.perezgomez@gmail.com>
2016-11-01 17:06:21 +0100

    Fix code input in new promotion code

--- app/overrides/promotions/manage_codes.rb
+++ app/overrides/promotions/manage_codes.rb
@@ -4,7 +4,7 @@ Deface::Override.new(:virtual_path => 'spree/admin/promotions/_form',
                      :name => 'hide_code_in_form',
                      :replace => 'erb[loud]:contains("f.field_container :code")',
                      :closing_selector => 'erb[silent]:contains("end")',
-                     :text => "<%= button_link_to(Spree.t('actions.manage_codes'), admin_promotion_promotion_codes_path(@promotion), { class: 'btn-success' }) %>")
+                     :text => "<%= button_link_to(Spree.t('actions.manage_codes'), admin_promotion_promotion_codes_path(@promotion), { class: 'btn-success' }) if @promotion.id %>")
 
 
 
