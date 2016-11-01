0391de4584443a892a14fa9991767903dc955971
epergo <ej.perezgomez@gmail.com>
2016-11-01 17:05:26 +0100

    Promotions without codes (not coupons) scope

--- app/models/spree/promotion_decorator.rb
+++ app/models/spree/promotion_decorator.rb
@@ -3,13 +3,16 @@ Spree::Promotion.class_eval do
   has_many :promotion_codes, dependent: :destroy
   alias_method :codes, :promotion_codes
 
-  scope :coupons, -> { joins(:promotion_codes).group("#{table_name}.id").having("count(*) > ?", 0) }
+  scope :coupons,    -> { where("id IN (SELECT DISTINCT(promotion_id) FROM #{promotion_codes_table})") }
+  scope :no_coupons, -> { where("id NOT IN (SELECT DISTINCT(promotion_id) FROM #{promotion_codes_table})") }
 
   self.whitelisted_ransackable_attributes = %w(path promotion_category_id)
 
-  def self.with_coupon_code(coupon_code)
-    promotion_codes_table = reflect_on_association(:promotion_codes).table_name
+  def self.promotion_codes_table
+    reflect_on_association(:promotion_codes).table_name
+  end
 
+  def self.with_coupon_code(coupon_code)
     joins("INNER JOIN #{promotion_codes_table}")
       .where("#{promotion_codes_table}.code = ?", coupon_code.strip.downcase)
       .first
