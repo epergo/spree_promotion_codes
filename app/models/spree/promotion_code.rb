3438740d5ec31aeedfda5e88b16d714f6a4441e9
epergo <eloy.perez@the-cocktail.com>
2016-11-03 17:34:59 +0100

    Helper to get how many times a code has been used

--- app/models/spree/promotion_code.rb
+++ app/models/spree/promotion_code.rb
@@ -3,5 +3,14 @@ module Spree
     belongs_to :promotion
 
     validates :code, uniqueness: true
+
+    def times_used
+      Spree::Adjustment
+        .joins("INNER JOIN spree_orders_promotions ON spree_orders_promotions.order_id = spree_adjustments.adjustable_id")
+        .eligible
+        .where(source_id: promotion_id)
+        .where("spree_orders_promotions.promotion_code_id = ?", id)
+    end
   end
 end
+
