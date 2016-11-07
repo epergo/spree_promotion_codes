26f438c3db86eca28a956de7248c9d0fbe11dccc
epergo <eloy.perez@the-cocktail.com>
2016-11-07 11:02:31 +0100

    Scope in Order to filter by promotion code used

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,23 @@
 Spree::Order.class_eval do
   has_many :orders_promotions
   has_many :promotions, through: :orders_promotions
+
+  def self.ransackable_scopes(auth_object = nil)
+    %w(with_promotion_code)
+  end
+
+  scope :with_promotion_code, -> (promotion_code) do
+    promotion_code = Spree::PromotionCode.find_by(code: promotion_code.downcase)
+
+    return unless promotion_code
+
+    joins(:adjustments)
+    .merge(Spree::Adjustment.eligible)
+    .joins("INNER JOIN #{orders_promotions_table} ON #{orders_promotions_table}.order_id = spree_adjustments.adjustable_id")
+    .where("#{orders_promotions_table}.promotion_code_id = ?", promotion_code.id)
+  end
+
+  def self.orders_promotions_table
+    reflect_on_association(:orders_promotions).table_name
+  end
 end
